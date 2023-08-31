86de0a0089932eeef6d29d6486bc5729f2195f54
Giovannyhs <giovannyhs@outlook.com>
2023-08-30 23:28:29 -0500

    cmabios de inicio

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -7,12 +7,18 @@ import Footer from "../Componentes/Footer";
 function Inicio() {
   return (
     <div className="Inicio_container">
+      <Header />
+      <Footer />
+      <Back />
+      <Luna />
+      <h2>hola</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vero
+        perspiciatis non temporibus aliquid sequi beatae molestias, a dolorum
+        nisi id, iste repudiandae porro alias quisquam? Fugit, iusto facilis.
+        Odit.
+      </p>
       <div className="inicio">
-        <Header />
-        <Footer />
-        <Back />
-        <Luna />
-
         <h2>hola</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vero
